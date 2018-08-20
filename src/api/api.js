4fbcf690f77b23f7e6450c8e7b23ec1fc145ce34
DouXubo <douxubo@jdab119.com>
2018-08-20 09:04:35 +0800

    add

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,10 @@
-import axios from 'axios'
 
 
 let base = 'http://api.xinyo.xin/api';
 
 /**登录接口**/
 export const Login = params => {
-    return axios.get(`${base}/Common/Login`, {
+    return this.$axios.get(`${base}/Common/Login`, {
         params: params
     }).then(res => res);
 };
@@ -13,24 +12,24 @@ export const Login = params => {
  * 火车票相关接口
  */
 export const gettrainlist = params => {
-    return axios.get(`${base}/TrainInformation/SearchList`, {
+    return this.$axios.get(`${base}/TrainInformation/SearchList`, {
         params: params
     }).then(res => res);
 };
 export const addTrain = params => {
-    return axios.post(`${base}/TrainInformation/add`,
+    return this.$axios.post(`${base}/TrainInformation/add`,
         params
     );
 };
 
 export const editTrain = params => {
-    return axios.post(`${base}/TrainInformation/edit`,
+    return this.$axios.post(`${base}/TrainInformation/edit`,
         params
     );
 };
 
 export const deleteTrain = params => {
-    return axios.delete(`${base}/TrainInformation/delete`, {
+    return this.$axios.delete(`${base}/TrainInformation/delete`, {
         params: params
     }).then(res => res);
 };
@@ -39,18 +38,18 @@ export const deleteTrain = params => {
  * 日常消费相关接口
  */
 export const getDailyConsumptionlist = params => {
-    return axios.get(`${base}/CapitalFlow/SearchList`, {
+    return this.$axios.get(`${base}/CapitalFlow/SearchList`, {
         params: params
     }).then(res => res);
 };
 export const addDailyConsumption = params => {
-    return axios.post(`${base}/CapitalFlow/add`, params);
+    return this.$axios.post(`${base}/CapitalFlow/add`, params);
 };
 export const editDailyConsumption = params => {
-    return axios.post(`${base}/CapitalFlow/edit`, params);
+    return this.$axios.post(`${base}/CapitalFlow/edit`, params);
 };
 export const deleteDailyConsumption = params => {
-    return axios.delete(`${base}/CapitalFlow/delete`, {
+    return this.$axios.delete(`${base}/CapitalFlow/delete`, {
         params: params
     }).then(res => res);
 };
